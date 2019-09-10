97931d340e98861cc979ae6612262761e18f4af7
Romain Gadrat <gadrat.romain33@gmail.com>
2019-09-10 17:39:13 +0200

    (EDIT) link api prod modifié

--- src/sagaRequest/index.js
+++ src/sagaRequest/index.js
@@ -1,2 +1,2 @@
 export const DEV = process.env.NODE_ENV === 'development' ? true : false;
-export const URL = DEV === true ? "http://51.38.187.216:9090" : "http://51.38.187.216:9000/api/v1"
+export const URL = DEV === true ? "http://51.38.187.216:9090" : "https://api.dune-table.com/"
